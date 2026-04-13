384fb9cbf6ac91dc22bfda2f94eee0d5f552cb17
ashton <563398@escg.ac.uk>
2026-04-13 15:57:36 +0100

    added animations and started dash

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,21 +1,48 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Transform groundCheck;
     public LayerMask groundLayer;
+    Animator anim;
 
-    private float horizontal;
     private bool isFacingRight = true;
+    private float horizontal;
+
     [Header("player values")]
     [SerializeField] float speed = 8f;
     [SerializeField] float jumpingPower = 16;
     
+    [Header("dashing values")]
+    public float dashSpeed = 20f;
+    public float dashDuration = 0.1f;
+    public float dashCooldown = 0.1f;
+    [SerializeField] private TrailRenderer tr;
+
+    bool isDashing;
+    bool canDash = true;
+    TrailRenderer trailRenderer;
+
+    InputAction moveAction;
+    InputAction jumpAction;
+    InputAction dashAction;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+        dashAction = InputSystem.actions.FindAction("Dash");
+    }
 
     private void Update()
     {
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        Jump();
+        Move();
+        Dash();
 
         if (isFacingRight && horizontal > 0f)
         {
@@ -25,23 +52,31 @@ public class PlayerMovement : MonoBehaviour
         {
             Flip();
         }
-    }
 
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed && IsGrounded())
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x,jumpingPower);
-        }
-        if (context.canceled && rb.linearVelocity.y > 0f)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
-        }
-    } 
+        //walking animation
+        anim.SetFloat("Speed", Mathf.Abs(horizontal));
 
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        //jumping animation
+        float verticalVelocity = rb.linearVelocity.y;
+
+        if (IsGrounded())
+        {
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isFalling", false);
+        }
+        else
+        {
+            if (verticalVelocity > 0.1f)
+            {
+                anim.SetBool("isJumping", true);
+                anim.SetBool("isFalling", false);
+            }
+            else if (verticalVelocity < -0.1f)
+            {
+                anim.SetBool("isJumping", false);
+                anim.SetBool("isFalling", true);
+            }
+        }
     }
 
     private void Flip()
@@ -52,8 +87,46 @@ public class PlayerMovement : MonoBehaviour
         transform.localScale = localScale;
     }
 
-    public void Move(InputAction.CallbackContext context)
+    private bool IsGrounded()
     {
-        horizontal = context.ReadValue<Vector2>().x;
+        bool grounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        if (grounded)
+        {
+            
+        }
+        return grounded;
+    }
+
+    public void Move()
+        {
+            horizontal = moveAction.ReadValue<Vector2>().x;
+            rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        }
+
+    public void Jump()
+    {
+        if (jumpAction.IsPressed() && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x,jumpingPower);
+            
+            print("jumped");
+        }
+        if (jumpAction.IsPressed()==false && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            
+            print("falling");
+        }
+    }
+
+    public void Dash()
+    {
+        if (dashAction.IsPressed() && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2 (rb.linearVelocity.x, dashSpeed);
+
+            print("dashed");
+        }
     }
 }
